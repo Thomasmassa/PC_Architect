5dd08fae66b84297980c5c6e9bdb4a97fb5dea6d
Thomasmassa <126335279+Thomasmassa@users.noreply.github.com>
2024-03-24 17:15:23 +0100

    .

---
+++ PC_ArchitectInstaller/PC_ArchitectInstaller/Models/CPU_Cooler.cs
@@ -0,0 +1,14 @@
+﻿namespace PC_ArchitectInstaller.Models
+{
+    public class CPU_Cooler
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Size { get; set; }
+        public string Color { get; set; } = "";
+        public int[] Rpm { get; set; }
+        public int[] Airflow { get; set; }
+        public int[] Noise_level { get; set; }
+        public bool Pwm { get; set; }
+    }
+}
