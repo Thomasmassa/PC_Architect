431048bce80659f3a9aac766fd59faf9ef495e76
Thomasmassa <126335279+Thomasmassa@users.noreply.github.com>
2024-04-22 12:09:01 +0200

    zalm code aangepast swipeview

--- PcArchitect/PcArchitect/ViewModel/StartBuildingViewModel.cs
+++ PcArchitect/PcArchitect/ViewModel/StartBuildingViewModel.cs
@@ -13,7 +13,6 @@ namespace PcArchitect.ViewModel
         private readonly IComponentService _componentService;
         private readonly ComponentRepository _componentRepository;
         private readonly Root _root;
-
         public ObservableCollection<IComponent> Components { get; set; }
 
         public StartBuildingViewModel(IComponentService componentService, ComponentRepository componentRepository, Root root)
@@ -24,13 +23,13 @@ namespace PcArchitect.ViewModel
             Components = new ObservableCollection<IComponent>();   
         }
 
-        public Task AddPresets()
+        public Task AddComponents()
         {
             return Task.Run(() =>
             {
                 var lastCpu = _root.Cpu.Where(cpu => cpu != null).LastOrDefault();
-                Components.Add(new Cpu { Name = lastCpu.Name, Image = lastCpu.Image });       
-                
+                Components.Add(new Cpu { Name = lastCpu.Name, Image = lastCpu.Image });
+
                 var lastCpuCooler = _root.CpuCooler.Where(cpuCooler => cpuCooler != null).LastOrDefault();
                 Components.Add(new CpuCooler { Name = lastCpuCooler.Name, Image = lastCpuCooler.Image });
 
@@ -47,7 +46,7 @@ namespace PcArchitect.ViewModel
                 Components.Add(new Ssd { Name = lastSsd.Name, Image = lastSsd.Image });
 
                 var lastHdd = _root.Hdd.Where(hdd => hdd != null).LastOrDefault();
-                Components.Add(new Hdd { Name = lastHdd.Name, Image = lastHdd.Image }); 
+                Components.Add(new Hdd { Name = lastHdd.Name, Image = lastHdd.Image });
 
                 var lastPsu = _root.Psu.Where(psu => psu != null).LastOrDefault();
                 Components.Add(new Psu { Name = lastPsu.Name, Image = lastPsu.Image });
@@ -66,10 +65,26 @@ namespace PcArchitect.ViewModel
         [RelayCommand]
         private async Task PageNavigated(NavigatedToEventArgs args)
         {
-            Components.Clear();
-            await AddPresets();
+            await AddComponents();
         }
 
+        //[RelayCommand]
+        //public async Task DeleteComponent (IComponent component)
+        //{
+        //    if (component == null)
+        //        return;
+
+        //    // Zoek de component in _root
+        //    var componentType = component.GetType();
+        //    var componentList = typeof(Root).GetProperty(componentType.Name).GetValue(_root);
+
+        //    // Verwijder de component
+        //    var componentToRemove = componentList.Cast<IComponent>().FirstOrDefault(c => c.Name == component.Name);
+        //    if (componentToRemove != null)
+        //    {
+        //        componentList.Remove(componentToRemove);
+        //    }
+        //}
 
         [RelayCommand]
         public async Task GoToPartsList(IComponent component)
@@ -77,6 +92,7 @@ namespace PcArchitect.ViewModel
             if (component == null)
                 return;
 
+            Components.Clear();
             // Navigeer naar de PartsList pagina
             await Shell.Current.GoToAsync(nameof(PartListPage), true, new Dictionary<string, object>
             {
