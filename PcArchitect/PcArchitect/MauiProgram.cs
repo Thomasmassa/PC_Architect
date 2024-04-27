0c22b6f16feaadd76cfaa1bcb91103ed5842047e
Thomasmassa <126335279+Thomasmassa@users.noreply.github.com>
2024-04-27 20:22:27 +0200

    Dictionary service aangemaakt voor de PartDetails en view aangepast

--- PcArchitect/PcArchitect/MauiProgram.cs
+++ PcArchitect/PcArchitect/MauiProgram.cs
@@ -33,6 +33,7 @@ namespace PcArchitect
             builder.Services.AddSingleton<StartBuildingPage>();
             builder.Services.AddSingleton<PartListViewModel>();
             builder.Services.AddSingleton<PartListPage>();
+            builder.Services.AddSingleton<BufferService>();
 
             builder.Services.AddTransient<PartDetailViewModel>();
             builder.Services.AddTransient<PartDetailPage>();
